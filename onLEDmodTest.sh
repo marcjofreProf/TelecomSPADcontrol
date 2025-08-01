e54b0997a3c5cdf2800c679473920ec5d718c07d
Demo User <debian@beaglebone.localdomain>
2025-08-01 10:56:54 +0000

    LED mod 2

--- onLEDmodTest.sh
+++ onLEDmodTest.sh
@@ -1,5 +1,6 @@
 # Script to modulate and LED for testing purposes with the PWM
 cd /sys/class/pwm/pwmchip4/pwm-4\:0
-sudo sh -c "echo '10000' >> ./period"
-sudo sh -c "echo '1000' >> ./duty_cycle" (with respect the period)
+sudo config-pin P9_14 pwm
+sudo sh -c "echo '1000000' >> ./period"
+sudo sh -c "echo '100000' >> ./duty_cycle"
 sudo sh -c "echo '1' >> ./enable"
