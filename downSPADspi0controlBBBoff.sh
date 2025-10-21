6782365d592977cb558056c13757f6caa29eb5a6
Demo User <debian@beaglebone.localdomain>
2025-10-21 16:47:59 +0000

    Disable relay 1

--- downSPADspi0controlBBBoff.sh
+++ downSPADspi0controlBBBoff.sh
@@ -14,15 +14,15 @@ sudo sh -c "echo '0' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '0' >> ./enable"
 
-# DC diode Schottky bias
-cd /sys/class/pwm/pwmchip1/pwm-1\:0
-sudo config-pin P9_22 pwm
-# Always at least set a period and duty cycle so that the pwm can be disabled
-sudo sh -c "echo '1000000000' >> ./period"
-sudo sh -c "echo '1000000000' >> ./duty_cycle"
-sudo sh -c "echo '0' >> ./enable"
-# Re-confirm order
-sudo sh -c "echo '0' >> ./enable"
+## DC diode Schottky bias
+#cd /sys/class/pwm/pwmchip1/pwm-1\:0
+#sudo config-pin P9_22 pwm
+## Always at least set a period and duty cycle so that the pwm can be disabled
+#sudo sh -c "echo '1000000000' >> ./period"
+#sudo sh -c "echo '1000000000' >> ./duty_cycle"
+#sudo sh -c "echo '0' >> ./enable"
+## Re-confirm order
+#sudo sh -c "echo '0' >> ./enable"
 
 # DC bias control disable
 cd /home/debian/Scripts/TelecomSPADcontrol 
