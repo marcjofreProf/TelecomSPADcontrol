556c0b44e8818c2295434879fde898ed264686ea
Demo User <debian@beaglebone.localdomain>
2025-10-22 15:39:09 +0000

    Improv 2

--- onLDmodTest.sh
+++ onLDmodTest.sh
@@ -1,8 +1,8 @@
 # Script to modulate a Telecom LD for testing purposes with the PWM
 cd /sys/class/pwm/pwmchip4/pwm-4\:0
 sudo config-pin P9_14 pwm
-sudo sh -c "echo '100' >> ./period"
-sudo sh -c "echo '50' >> ./duty_cycle"
+sudo sh -c "echo '10000' >> ./period"
+sudo sh -c "echo '2000' >> ./duty_cycle"
 sudo sh -c "echo '1' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '1' >> ./enable"
