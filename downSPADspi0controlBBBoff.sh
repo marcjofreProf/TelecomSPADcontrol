5e9f40c2c3a10eea4904051b744a3b91e36c040b
Demo User <debian@beaglebone.localdomain>
2025-10-29 09:12:47 +0000

    Improv gating

--- downSPADspi0controlBBBoff.sh
+++ downSPADspi0controlBBBoff.sh
@@ -8,8 +8,8 @@
 cd /sys/class/pwm/pwmchip7/pwm-7\:0
 sudo config-pin P8_19 pwm
 # Always at least set a period and duty cycle so that the pwm can be disabled
-sudo sh -c "echo '50' >> ./period"
-sudo sh -c "echo '5' >> ./duty_cycle"
+sudo sh -c "echo '10000' >> ./period"
+sudo sh -c "echo '5000' >> ./duty_cycle"
 sudo sh -c "echo '0' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '0' >> ./enable"
---
+++ onGatingTest.sh
@@ -0,0 +1,10 @@
+# Scrip to directly gate the system
+sudo config-pin P8_19 pwm
+
+cd /sys/class/pwm/pwmchip7/pwm-7\:0
+sudo sh -c "echo '1000000' >> ./period"
+sudo sh -c "echo '500000' >> ./duty_cycle"
+sudo sh -c "echo '1' >> ./enable"
+# Re-confirm order
+sudo sh -c "echo '1' >> ./enable"
+echo "Gating ON"
