f3bf78d76ff2e0e7495db26cce6867bd240a3ee3
Demo User <debian@beaglebone.localdomain>
2025-11-25 23:46:45 +0000

    Improv 1!

--- downSPADspi0controlBBBoff.sh
+++ downSPADspi0controlBBBoff.sh
@@ -9,7 +9,7 @@ cd /sys/class/pwm/pwmchip7/pwm-7\:0
 sudo config-pin P8_19 pwm
 # Always at least set a period and duty cycle so that the pwm can be disabled
 sudo sh -c "echo '2000' >> ./period"
-sudo sh -c "echo '1000' >> ./duty_cycle"
+sudo sh -c "echo '500' >> ./duty_cycle"
 sudo sh -c "echo '0' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '0' >> ./enable"
--- upSPADspi0controlBBBon.sh
+++ upSPADspi0controlBBBon.sh
@@ -17,7 +17,7 @@ cd /sys/class/pwm/pwmchip7/pwm-7\:0
 sudo config-pin P8_19 pwm
 # Always at least set a period and duty cycle so that the pwm can be disabled
 sudo sh -c "echo '2000' >> ./period"
-sudo sh -c "echo '1000' >> ./duty_cycle"
+sudo sh -c "echo '500' >> ./duty_cycle"
 sudo sh -c "echo '0' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '0' >> ./enable"
@@ -46,7 +46,7 @@ sudo config-pin P8_19 pwm
 if [[ "$answerGeiger" == "y" || "$answerGeiger" == "yes" ]]; then
     cd /sys/class/pwm/pwmchip7/pwm-7\:0
     sudo sh -c "echo '2000' >> ./period"
-    sudo sh -c "echo '1000' >> ./duty_cycle"
+    sudo sh -c "echo '500' >> ./duty_cycle"
     sudo sh -c "echo '1' >> ./enable"
     # Re-confirm order
     sudo sh -c "echo '1' >> ./enable"
@@ -55,7 +55,7 @@ else
     cd /sys/class/pwm/pwmchip7/pwm-7\:0
     # Always at least set a period and duty cycle so that the pwm can be disabled
     sudo sh -c "echo '2000' >> ./period"
-    sudo sh -c "echo '1000' >> ./duty_cycle"
+    sudo sh -c "echo '500' >> ./duty_cycle"
     sudo sh -c "echo '0' >> ./enable"
     # Re-confirm order
     sudo sh -c "echo '0' >> ./enable"
