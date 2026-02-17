29bd8102954360f4252faff68c24deadbd473d0c
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2026-02-17 12:10:04 +0100

    Improv logic 7

--- ManualControl/offLDmodTest.sh
+++ ManualControl/offLDmodTest.sh
@@ -4,7 +4,7 @@ cd /sys/class/pwm/pwmchip4/pwm-4\:0
 sudo config-pin P9_14 pwm
 # Always at least set a period and duty cycle so that the pwm can be disabled
 sudo sh -c "echo '50000' >> ./period"
-sudo sh -c "echo '40000' >> ./duty_cycle"
+sudo sh -c "echo '45000' >> ./duty_cycle"
 sudo sh -c "echo '0' >> ./enable"
 # Re- confirm order
 sudo sh -c "echo '0' >> ./enable"
--- ManualControl/onLDmodTest.sh
+++ ManualControl/onLDmodTest.sh
@@ -3,7 +3,7 @@
 cd /sys/class/pwm/pwmchip4/pwm-4\:0
 sudo config-pin P9_14 pwm
 sudo sh -c "echo '50000' >> ./period"
-sudo sh -c "echo '40000' >> ./duty_cycle"
+sudo sh -c "echo '45000' >> ./duty_cycle"
 sudo sh -c "echo '1' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '1' >> ./enable"
