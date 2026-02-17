763572d75a284ca30d9c309b006b35f7ae097e98
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2026-02-17 11:54:30 +0100

    Improv logic 3

--- ManualControl/onLDmodTest.sh
+++ ManualControl/onLDmodTest.sh
@@ -2,8 +2,8 @@
 # Script to modulate a Telecom LD for testing purposes with the PWM
 cd /sys/class/pwm/pwmchip4/pwm-4\:0
 sudo config-pin P9_14 pwm
-sudo sh -c "echo '1000000' >> ./period"
-sudo sh -c "echo '900000' >> ./duty_cycle"
+sudo sh -c "echo '10000' >> ./period"
+sudo sh -c "echo '9000' >> ./duty_cycle"
 sudo sh -c "echo '1' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '1' >> ./enable"
