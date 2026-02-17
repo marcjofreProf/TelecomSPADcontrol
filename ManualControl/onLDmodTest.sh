2df43d2ebb67f0730349aaacad2eb100ecc4c118
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2026-02-17 11:53:20 +0100

    Improv logic 2

--- ManualControl/onLDmodTest.sh
+++ ManualControl/onLDmodTest.sh
@@ -3,7 +3,7 @@
 cd /sys/class/pwm/pwmchip4/pwm-4\:0
 sudo config-pin P9_14 pwm
 sudo sh -c "echo '1000000' >> ./period"
-sudo sh -c "echo '100000' >> ./duty_cycle"
+sudo sh -c "echo '900000' >> ./duty_cycle"
 sudo sh -c "echo '1' >> ./enable"
 # Re-confirm order
 sudo sh -c "echo '1' >> ./enable"
