e3eeac38ed1cb74791bcfd67cf9ce927a859b351
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2026-02-18 10:05:27 +0100

    Improv 1

--- AutomaticControl/CppScripts/GPIOspadSYScont.h
+++ AutomaticControl/CppScripts/GPIOspadSYScont.h
@@ -104,7 +104,7 @@ private:// Variables
 	unsigned int* valp; // 32 bits
 
 	// PRU signals settings
-	unsigned int pru1_cycles_period = 256; // Initial number of clocks per cycle (it has to be power of 2)
+	unsigned int pru1_cycles_period = 512; // Initial number of clocks per cycle (it has to be power of 2). Sets the frequency of the geiger gating operation
 	unsigned int pru1_delay_first_off = 30; // Initial number of relative clocks/2 - 1 of the first off. At least 1.
 	unsigned int pru1_delay_second_off = 1; // Initial number of relative clocks/2 - 1 of the second off. At least 1.
 	unsigned int pru1_delay_third_off = 1; // Initial number of relative clocks/2 - 1 of the third off. At least 1.
@@ -143,7 +143,7 @@ private:// Variables
     const double MAX_DUTY = 0.4; // Limits to the values of duty cycle
     const double MAX_V_STEP = 0.75; // At least it has to be larger than 0.2V
     const double MAX_DC_STEP = 0.15; // At least it has to be larger than 0.05. Duty cycle step
-    const double DT = PRUmeasInterval/1000000000.0;  // Time Interval of counts measurment
+    const double DT = PRUmeasInterval/1000000000.0;  // Time Interval of counts measurement
 
     double duty_cycles[NumDetChannels];  // Current duty cycles, will be updated
 	double current_desired_voltage=0.0;  // Current voltage, will be updated
