2131addae1bbe699438bc25043588376f2f5156a
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2026-02-23 10:36:12 +0100

    Adj 3

--- AutomaticControl/CppScripts/GPIOspadSYScont.cpp
+++ AutomaticControl/CppScripts/GPIOspadSYScont.cpp
@@ -528,7 +528,7 @@ int GPIO::calculateSPADControl(){
         	if (total_cps>0.0){ // Update values if different than 0
             	inflection_counter++;
             }
-            if (inflection_counter >= 3) {
+            if (inflection_counter >= 5) { // Number of checks to consider that it has surpassed the inflection point
                 past_inflection_point = true;
                 cout << "Detected passing voltage inflection point. Implementing voltage drop correction!"<< endl;
             }
